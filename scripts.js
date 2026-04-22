da34e2e9d1f5ed7e6f958b194b2743630011ccd0
lse2001 <lucas.seitz@gmail.com>
2026-04-21 20:46:15 -0700

    switch to pokemon data, update rendering, and fix layout and images

--- scripts.js
+++ scripts.js
@@ -1,43 +1,149 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+const cards = [
+  {
+    "id": 1,
+    "name": "Bulbasaur",
+    "type": "Grass",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "001",
+    "image": "images/bulbasaur.png"
+  },
+  {
+    "id": 2,
+    "name": "Ivysaur",
+    "type": "Grass",
+    "rarity": "Uncommon",
+    "owned": false,
+    "cardNumber": "002",
+    "image": "images/ivysaur.png"
+  },
+  {
+    "id": 3,
+    "name": "Venusaur",
+    "type": "Grass",
+    "rarity": "Rare",
+    "owned": false,
+    "cardNumber": "003",
+    "image": "images/venusaur.png"
+  },
+  {
+    "id": 4,
+    "name": "Charmander",
+    "type": "Fire",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "004",
+    "image": "images/charmander.png"
+  },
+  {
+    "id": 5,
+    "name": "Charmeleon",
+    "type": "Fire",
+    "rarity": "Uncommon",
+    "owned": false,
+    "cardNumber": "005",
+    "image": "images/charmeleon.png"
+  },
+  {
+    "id": 6,
+    "name": "Charizard",
+    "type": "Fire",
+    "rarity": "Rare",
+    "owned": false,
+    "cardNumber": "006",
+    "image": "images/charizard.png"
+  },
+  {
+    "id": 7,
+    "name": "Squirtle",
+    "type": "Water",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "007",
+    "image": "images/squirtle.png"
+  },
+  {
+    "id": 8,
+    "name": "Wartortle",
+    "type": "Water",
+    "rarity": "Uncommon",
+    "owned": false,
+    "cardNumber": "008",
+    "image": "images/wartortle.png"
+  },
+  {
+    "id": 9,
+    "name": "Blastoise",
+    "type": "Water",
+    "rarity": "Rare",
+    "owned": false,
+    "cardNumber": "009",
+    "image": "images/blastoise.png"
+  },
+  {
+    "id": 10,
+    "name": "Caterpie",
+    "type": "Bug",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "010",
+    "image": "images/caterpie.png"
+  },
+  {
+    "id": 11,
+    "name": "Metapod",
+    "type": "Bug",
+    "rarity": "Uncommon",
+    "owned": false,
+    "cardNumber": "011",
+    "image": "images/metapod.png"
+  },
+  {
+    "id": 12,
+    "name": "Butterfree",
+    "type": "Bug",
+    "rarity": "Rare",
+    "owned": false,
+    "cardNumber": "012",
+    "image": "images/butterfree.png"
+  },
+  {
+    "id": 13,
+    "name": "Weedle",
+    "type": "Bug",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "013",
+    "image": "images/weedle.png"
+  },
+  {
+    "id": 14,
+    "name": "Kakuna",
+    "type": "Bug",
+    "rarity": "Uncommon",
+    "owned": false,
+    "cardNumber": "014",
+    "image": "images/kakuna.png"
+  },
+  {
+    "id": 15,
+    "name": "Beedrill",
+    "type": "Bug",
+    "rarity": "Rare",
+    "owned": false,
+    "cardNumber": "015",
+    "image": "images/beedrill.png"
+  },
+  {
+    "id": 25,
+    "name": "Pikachu",
+    "type": "Electric",
+    "rarity": "Common",
+    "owned": true,
+    "cardNumber": "025",
+    "image": "images/pikachu.png"
+  }
+]
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -45,42 +151,34 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+  for (let i = 0; i < cards.length; i++) {
+    let pokemon = cards[i];
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, pokemon); // Edit card content
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+// This function fills in the card content
+function editCardContent(card, pokemon) {
   card.style.display = "block";
 
+  // Set name
   const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  cardHeader.textContent = pokemon.name;
 
+  // Set image
   const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  cardImage.src = pokemon.image;
+  cardImage.alt = pokemon.name;
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  // Set list items
+  const listItems = card.querySelectorAll("li");
+  listItems[0].textContent = "Type: " + pokemon.type;
+  listItems[1].textContent = "Rarity: " + pokemon.rarity;
+  listItems[2].textContent = pokemon.owned ? "Owned" : "Missing";
 }
-
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
